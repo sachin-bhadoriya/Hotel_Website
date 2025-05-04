5c2bab81bf6a2d2f5aa7d6ae9db3ba1058d69de7
sachin <sachin@singh.co.in>
2025-05-05 00:18:05 +0530

    fix some error and run only type npm run dev on root folder using concorrntly

--- admin-frontend/src/Pages/AboutFolder.jsx
+++ admin-frontend/src/Pages/AboutFolder.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-// import AddNote from '../components/AboutNote/AddAboutContent';
+import AddNote from '../components/AboutNote/AddAboutContent';
 import NoteList from '../components/AboutNote/AboutContentList';
 import axios from 'axios';
 
@@ -17,7 +17,7 @@ const AboutFolder = () => {
 
   return (
     <div>
-      {/* <AddNote refreshAboutTexts={fetchAboutTexts} /> */}
+      <AddNote refreshAboutTexts={fetchAboutTexts} />
       <NoteList abouttexts={abouttexts} refreshAboutTexts={fetchAboutTexts} />
     </div>
   );
